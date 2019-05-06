80810163fdcce95ab34219f78a597c5a095d372d
dheyme-jensen <dheymejensen@hotmail.com>
2019-05-06 19:22:30 -0300

    código refatorado

--- index.js
+++ index.js
@@ -1,37 +1,24 @@
-function cpfIncome(string){
-    const cpf  = string.split('').map(Number);
-    return [multiply(cpf, 9, 10), multiply(cpf, 10, 11)];   
+function getValidationDigits(cpf){
+    const cpfArray = cpf.split('').map(Number);
+    return [calculateDigit(cpfArray, 9, 10), calculateDigit(cpfArray, 10, 11)];  
 };
 
-function cpfLength(cpf){
-    return (cpf.length === 11)
-};
-
-function sameNumber(cpf){
-    return !( cpf === "00000000000" || cpf === "11111111111" || cpf === "22222222222" || cpf === "33333333333" ||
-     cpf === "44444444444" || cpf === "55555555555" || cpf === "66666666666" || cpf === "77777777777" ||
-     cpf === "88888888888" || cpf === "99999999999")
-};
-
-function multiply(cpf, sliceNum, mult){
-    const multiplying = cpf.splice(0, sliceNum).map((currElement,index) => { let x = mult - index;
-      return currElement * x;
-    });
-    const digit = sum(multiplying);
-    return digit;
-};
-   
-function sum(cpf){
-    const cpfSum = cpf.reduce((counter, element) => {return counter + element});
-    const cpfModule = (cpfSum * 10) % 11 % 10;
-    return cpfModule
+function calculateDigit(cpf, sliceNum, mult){
+    const multipliedDigits = cpf.splice(0, sliceNum).map((currElement,index) => currElement * (mult - index));
+    return multipliedDigits.reduce((counter, element) => counter + element) * 10 % 11 % 10;
 };
 
 function cpfValidator(cpf){
-    const cpfNumber = cpfIncome(cpf);
-    const isFirstEqual = cpfNumber[0] === parseInt(cpf[9]);
-    const isSecondEqual = cpfNumber[1] === parseInt(cpf[10]);
-    return ((isFirstEqual && isSecondEqual) && sameNumber(cpf) && cpfLength(cpf));   
+    const isSameNumber = cpf.split("").every((item) => item === cpf[0]);
+    const hasValidLength = cpf.length === 11;
+
+    if (!isSameNumber && hasValidLength) {
+        const validationDigits = getValidationDigits(cpf);
+        const isFirstEqual = validationDigits[0] === parseInt(cpf[9]);
+        const isSecondEqual = validationDigits[1] === parseInt(cpf[10]);
+        return (isFirstEqual && isSecondEqual);
+    }
+    return false;
 };
    
 console.log(cpfValidator('87052536080'));
