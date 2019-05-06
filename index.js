bc240891beb86275ad842231bfbb2ad532a855d2
dheyme-jensen <dheymejensen@hotmail.com>
2019-05-06 16:41:54 -0300

    versão 0.0.2

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ function cpfIncome(string){
     return [multiply(cpf, 9, 10), multiply(cpf, 10, 11)];   
 };
 
+function cpfLength(cpf){
+    return (cpf.length === 11)
+};
+
+function sameNumber(cpf){
+    return !( cpf === "00000000000" || cpf === "11111111111" || cpf === "22222222222" || cpf === "33333333333" ||
+     cpf === "44444444444" || cpf === "55555555555" || cpf === "66666666666" || cpf === "77777777777" ||
+     cpf === "88888888888" || cpf === "99999999999")
+};
+
 function multiply(cpf, sliceNum, mult){
     const multiplying = cpf.splice(0, sliceNum).map((currElement,index) => { let x = mult - index;
       return currElement * x;
